b1fdc42284b4abe1a55d61d43610da9ab73be389
Herman Nauyokas <nauyokas.lt@gmail.com>
2023-02-10 02:37:30 +0300

    Updated bootstrapper

--- frontline-website-load-time-script/README.md
+++ frontline-website-load-time-script/README.md
@@ -1,7 +1,15 @@
 ### python load-time-script.py loading_test.xlsx --loops 3
 ### python load-time-script.py loading_test.xlsx --loops 3 --disable_save
 ## To run all files simultaneously:
-### python bootstrapper.py --disable_save
+### flags
+#### --path - A path or a list of paths
+#### --recurse - Allows to get files and nested files from a directory
+#### --disable_save
+#### --loops
+### python bootstrapper.py --path . --disable_save
+### python bootstrapper.py --path . --disable_save --recurse
+### python bootstrapper.py --path TX-DEV TX-STG --disable_save --recurse
+### python bootstrapper.py --path TX-DEV TX-STG loading-test.xlsx --disable_save --recurse
 ### python bootstrapper.py --disable_save --loops 3
 ## Libraries to install
 ### pip install numpy
--- frontline-website-load-time-script/bootstrapper.py
+++ frontline-website-load-time-script/bootstrapper.py
@@ -2,25 +2,51 @@ import os
 import subprocess
 import argparse
 
-def get_excel_files_in_dir(base_path):
+def get_excel_files_in_dir(base_path, recurse=False):
 	result_files = []
 	for (root,dirs,files) in os.walk(base_path, topdown=True):
 		for file in files:
 			if "xlsx" in file:
 				result_files.append(f"{root}\{file}")
+		if not recurse:
+			break
 	return result_files
+
+def get_new_unique_files(new_files, existing_files):
+	unique_files = []
+	for new_file in new_files:
+		is_file_exists = False
+		for existing_file in existing_files:
+			if os.path.samefile(new_file, existing_file):
+				is_file_exists = True
+				break
+		if not is_file_exists:
+			unique_files.append(new_file)
+	return unique_files
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--loops", type=int, default=3)
 	parser.add_argument("--disable_save", action="store_true")
+	parser.add_argument("--recurse", action="store_true")
+	parser.add_argument('-p', '--path', default=get_excel_files_in_dir("."), nargs='+')
 	my_namespace = parser.parse_args()
-	files = get_excel_files_in_dir(".")
+
+	files = []
+	for file in my_namespace.path:
+		new_files = []
+		if os.path.isfile(file):
+			new_files.append(file)
+
+		if os.path.isdir(file):
+			new_files += get_excel_files_in_dir(file, my_namespace.recurse)
+		files += get_new_unique_files(new_files, files)
+
 	for file in files:
 		command = f"start cmd /k python load-time-script.py {file} --loops {my_namespace.loops}"
 		if my_namespace.disable_save:
 			command += " --disable_save"
 		subprocess.call(command, shell=True)
-
+	print(files)
 if __name__ == "__main__":
 	main()
