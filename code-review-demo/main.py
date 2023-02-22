b64b7449b790641266cf462b061653b656a69338
Herman Nauyokas <nauyokas.lt@gmail.com>
2023-02-22 19:10:57 +0300

    Updated code review demo

--- code-review-demo/main.py
+++ code-review-demo/main.py
@@ -1,15 +1,12 @@
 import openai
+import os
 
-def main():
-    openai.api_key = "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"
+def get_code_review(code):
+    openai.api_key = os.environ.get("OPENAI_API_TOKEN")
     model_engine = "text-davinci-003"
     preprompt = ""
     with open("preprompt.txt", "r") as file:
         preprompt = file.read()
-
-    code = ""
-    with open("code-sample.txt", "r") as file:
-        code = file.read()
 
     prompt = f"{preprompt}\n{code}"
 
@@ -22,8 +19,14 @@ def main():
         frequency_penalty=0,
         presence_penalty=0
     )
+    return completion.choices[0].text
 
-    print(completion.choices[0].text)
+def main():
+    code = ""
+    with open("code-sample.txt", "r") as file:
+        code = file.read()
+
+    print(get_code_review(code))
 
 if __name__ == "__main__":
     main()
