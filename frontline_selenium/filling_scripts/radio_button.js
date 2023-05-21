e3131f7063fc354a875c391ac80e1053a294e3e2
Herman Nauyokas <nauyokas.lt@gmail.com>
2023-05-21 22:19:20 +0300

    Added radio button filler for planng

--- frontline_selenium/filling_scripts/radio_button.js
+++ frontline_selenium/filling_scripts/radio_button.js
@@ -13,5 +13,16 @@ if (isPlanPage) {
         groups[keys[i]][Math.floor(Math.random() * groups[keys[i]].length)].click();
     }
 }else {
-
+    groups = {};
+    $("input[type='radio']").each(function() {
+        var groupName = $(this).attr('id').split('_')[0];
+        if (!(groupName in groups)) {
+            groups[groupName] = [];
+        }
+        groups[groupName].push($(this));
+    });
+    var keys = Object.keys(groups);
+    for (var i in keys) {
+        groups[keys[i]][Math.floor(Math.random() * groups[keys[i]].length)].click();
+    }
 }
