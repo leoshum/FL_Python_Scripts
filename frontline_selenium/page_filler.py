ef25ef686343a6b407122961197a3c424618316d
Herman Nauyokas <nauyokas.lt@gmail.com>
2023-05-28 01:03:45 +0300

    Fixed dropdown filler blocking bug and added datetime picker filler

--- frontline_selenium/page_filler.py
+++ frontline_selenium/page_filler.py
@@ -1,7 +1,10 @@
 import os
 import random
+import time
+import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from frontline_selenium.selenium_helper import SeleniumHelper
 from frontline_selenium.random_html_generator import RandomHtmlGenerator
 from faker import Faker
@@ -18,7 +21,7 @@ class PageFormFiller:
         PageFormFiller.fill_form_textboxes(driver)
         PageFormFiller.fill_form_textareas(driver)
         PageFormFiller.fill_form_rich_text_editors(driver)
-        PageFormFiller.fill_form_date_time_pickes(driver)
+        PageFormFiller.fill_form_date_time_picker(driver)
     
     def create_script(file_name: str, params: dict) -> str:
         with open(PageFormFiller.scripts_directory + file_name, "r") as script_file:
@@ -93,25 +96,32 @@ class PageFormFiller:
                     input.send_keys(data_keys[random.randint(0, len(data_keys) - 1)])
                 except:
                     pass
-                
-            dropdownlists = driver.find_elements(By.CSS_SELECTOR, "kendo-dropdownlist")
+            dropdownlists = driver.find_elements(By.CSS_SELECTOR, "table kendo-dropdownlist")
             for dropdownlist in dropdownlists:
-                dropdownlist.click()
                 try:
+                    dropdownlist.click()
                     popup = driver.find_element(By.CSS_SELECTOR, "kendo-popup")
-                    print(popup.get_attribute('innerHTML'))
                     options = popup.find_elements(By.CSS_SELECTOR, "ul>li")
+                    print(options)
                     options[random.randint(0, len(options) - 1)].click()
                 except:
                     pass
 
     @staticmethod
-    def fill_form_date_time_pickes(driver: webdriver.Chrome) -> None:
-        script = PageFormFiller.create_script("date_time_picker.js", {
-            "{{isPlanPage}}": str(SeleniumHelper.is_plan_page_url(driver.current_url)).lower()
-        })
-        driver.execute_script(script)
-    
+    def fill_form_date_time_picker(driver: webdriver.Chrome) -> None:
+        if SeleniumHelper.is_plan_page_url(driver.current_url):
+            script = PageFormFiller.create_script("date_time_picker.js", {})
+            driver.execute_script(script)
+        else:
+            date_pickers = driver.find_elements(By.CSS_SELECTOR, "kendo-datepicker")
+            for date_picker in date_pickers:
+                try:
+                    input = date_picker.find_element(By.CSS_SELECTOR, "kendo-dateinput>input")
+                    next_day_date = datetime.datetime.today() + datetime.timedelta(days=1)
+                    input.send_keys(str(next_day_date.year), Keys.ARROW_LEFT, str(next_day_date.day), Keys.ARROW_LEFT, Keys.ARROW_LEFT, str(next_day_date.month))
+                except:
+                    pass
+                  
     @staticmethod
     def fill_form_multiple_selects(driver: webdriver.Chrome) -> None:
         script = PageFormFiller.create_script("multiple_select.js", {
