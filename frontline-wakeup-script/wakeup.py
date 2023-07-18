e11ab0d2ce26ea6133fcb7dc5731f0de8aeff601
Herman Nauyokas <nauyokas.lt@gmail.com>
2023-07-19 01:02:19 +0300

    wakeup script updated

--- frontline-wakeup-script/wakeup.py
+++ frontline-wakeup-script/wakeup.py
@@ -1,28 +1,34 @@
 import os
 import re
+import sys
 import json
 import logging
 import requests
 import argparse
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from urllib.error import HTTPError
+from datetime import datetime
+
 
 def extract_base_url(url):
-	url_parts = urlparse(url)
-	return f"{url_parts.scheme}://{url_parts.netloc}"
+    url_parts = urlparse(url)
+    return f"{url_parts.scheme}://{url_parts.netloc}"
 
 
-def get_support_tool_session():
+def get_support_tool_session(username, password):
     session = requests.Session()
     session.headers.update({
         'Content-Type': 'application/json'
     })
     base_url = "https://support-tech.acceliplan.com"
     data = {
-        "username": "SupportAdmin",
-        "password": "GftOc244Qw^j"
+        "username": username,
+        "password": password
     }
-    token = json.loads(session.post(f"{base_url}/api/auth", json=data).text)["token"]
+    response = session.post(f"{base_url}/api/auth", json=data)
+    response.raise_for_status()
+    token = json.loads(response.text)["token"]
     session.headers.update({
         'Authorization': f"Bearer {token}"
     })
@@ -40,7 +46,8 @@ def get_magic_link(session, host, username):
 def get_accelify_session_using_support_tool(support_tool_session, base_url, username):
     link = get_magic_link(support_tool_session, base_url, username)
     session = requests.Session()
-    session.get(link)
+    response = session.get(link)
+    response.raise_for_status()
     return session
 
 
@@ -82,18 +89,32 @@ def get_endpoints_for_planng(url, planng_mappings):
             for endpoint in mapping["endpoints"]:
                 endpoints.append(endpoint.format(base_url, *data))
             return endpoints
-    raise ValueError("there is no regex matches url")
+    raise ValueError(f"there is no regex in mapping matches {url}")
 
 
 def filter_urls(urls, sites):
     if sites:
         for site in sites:
-            if not urls.get(site): raise ValueError(f"Given site name '{site}' doesn't exist in the urls file!")
+            if not urls.get(site): raise ValueError(f"Parameter '{site}' doesn't exist in the urls file!")
         urls = dict(filter(lambda pair: pair[0] in sites, urls.items()))
     return urls
 
 
+def log_uncaught_exceptions(exctype, value, traceback):
+    logging.exception("Uncaught exception occurred: ", exc_info=(exctype, value, traceback))
+
+
 def main():
+    formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    logging.basicConfig(
+        filename=f"script__{formatted_datetime}.log",
+        level=logging.DEBUG,
+        format='%(asctime)s - %(message)s',
+    )
+    logging.getLogger('requests').setLevel(logging.CRITICAL)
+    logging.getLogger('urllib3').setLevel(logging.CRITICAL)
+    sys.excepthook = log_uncaught_exceptions
+
     parser = argparse.ArgumentParser()
     parser.add_argument("urls", type=str)
     parser.add_argument('--sites', nargs='+')
@@ -107,23 +128,40 @@ def main():
     with open(os.path.dirname(__file__) + "/planng-mappings.json") as file:
         planng_mappings = json.load(file)
 
-    urls = filter_urls(urls, sites) # make logging
+    with open(os.path.dirname(__file__) + "/config.json") as file:
+        config = json.load(file)
 
-    support_tool_session = get_support_tool_session()
+    urls = filter_urls(urls, sites)
+        
+    support_tool_session = get_support_tool_session(config["support_tool"]["username"]
+                                                  , config["support_tool"]["password"])
     for site_name in urls.keys():
-        accelify_session = get_accelify_session_using_support_tool(support_tool_session, extract_base_url(urls[site_name][0]), "pmgmt")
+        try:
+            base_url = extract_base_url(urls[site_name][0])
+            accelify_session = get_accelify_session_using_support_tool(support_tool_session
+                                                                     , base_url
+                                                                     , config["fl_credentials"]["username"])
+        except HTTPError as ex:
+            logging.error(f"Error occured while authenticating in {base_url}")
+            logging.exception(ex)
+            continue
+        
         for url in urls[site_name]:
             url = url.strip()
             if "planng" in url:
-                endpoints = get_endpoints_for_planng(url, planng_mappings) # make logging
+                try:
+                    endpoints = get_endpoints_for_planng(url, planng_mappings)
+                except ValueError as ex:
+                    logging.error(ex.args[0])
+                    continue
+
+                logging.info(f"{url}")
                 for endpoint in endpoints:
-                    accelify_session.get(endpoint)
-                    # make logging
-                    print(endpoint)
+                    response = accelify_session.get(endpoint)
+                    logging.info(f"    {response.status_code} - {endpoint}")
             else:
-                accelify_session.get(url)
-                # make logging
-                print(url)
+                response = accelify_session.get(url)
+                logging.info(f"{response.status_code} - {url}")
 
 if __name__ == "__main__":
     main()
