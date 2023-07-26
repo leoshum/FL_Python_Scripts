2c885ddefc701455c0c08beb9253870b593e62a4
Herman Nauyokas <nauyokas.lt@gmail.com>
2023-07-26 22:24:18 +0300

    Changed form filler

--- frontline_selenium/random_html_generator.py
+++ frontline_selenium/random_html_generator.py
@@ -1,3 +1,7 @@
+import os
+import base64
+#from PIL import Image
+#import io
 from faker import Faker
 import random
 
@@ -13,7 +17,7 @@ class RandomHtmlGenerator:
         random_tag = random.choice(html_tags)
 
         if random_tag == "<img":
-            attributes = f"src='{fake.image_url()}' alt='{fake.word()}'"
+            attributes = f"src='{RandomHtmlGenerator.get_random_image_link()}' alt='{fake.word()}'"
             html = f"{random_tag} {attributes} />"
         elif random_tag == "<a":
             attributes = f"href='{fake.url()}'"
@@ -33,3 +37,25 @@ class RandomHtmlGenerator:
             html += RandomHtmlGenerator.generate_random_html(depth + 1)
 
         return html
+
+    @staticmethod
+    def get_random_image_link():
+        fake = Faker()
+        dir_path = os.path.abspath(os.path.dirname(__file__)) + "\\cats\\"
+        image_id = fake.random_int(min=1, max=21)
+        img_path = f"{dir_path}cat_{image_id}.jpg"
+        with open(img_path, "rb") as image_file:
+            base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+        return f"data:image/jpg;base64, {base64_string}"
+
+#def main():
+#    dir_path = os.path.abspath(os.path.dirname(__file__)) + "\\cats\\"
+#    for filename in os.listdir(dir_path):
+#        print(filename)
+#        image = Image.open(dir_path + filename)
+#        new_width = image.width // 2
+#        new_height = image.height // 2
+#        resized_image = image.resize((new_width, new_height))
+#        resized_image.save(dir_path + filename, "JPEG", quality=70)
+#if __name__ == "__main__":
+#    main()
