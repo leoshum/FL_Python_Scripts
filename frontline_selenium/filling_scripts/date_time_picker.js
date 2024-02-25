ce1c11525bddc3c0922f3e9155da14248297e28f
German Naujokas <g.naujokas@click2.money>
2024-02-26 01:40:57 +0300

    random date choosing logic updated

--- frontline_selenium/filling_scripts/date_time_picker.js
+++ frontline_selenium/filling_scripts/date_time_picker.js
@@ -2,7 +2,15 @@ $(".form input[data-role='datePicker']").each(function () {
     var datepicker = $(this).data("kendoDatePicker");
     var minDate = new Date(datepicker.options.min).getTime();
     var maxDate = new Date(datepicker.options.max).getTime();
-    var randomTimestamp = minDate + Math.random() * (maxDate - minDate);
+
+    var currentDate = new Date().getTime();
+
+    if (currentDate >= minDate && currentDate <= maxDate) {
+        var randomOffset = Math.random() * (maxDate - currentDate);
+        var randomTimestamp = currentDate + randomOffset;
+    } else {
+        var randomTimestamp = minDate + Math.random() * (maxDate - minDate);
+    }
     datepicker.value(new Date(randomTimestamp));
     datepicker.trigger('change');
-})
+});
