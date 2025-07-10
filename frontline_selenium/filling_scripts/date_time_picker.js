e6a75dbfc03f019d6e275a6d730676aa3c6c9465
Artur Hart <artur.gart99@gmail.com>
2025-07-10 21:24:41 +0300

    fix: date time picker min and max value

--- frontline_selenium/filling_scripts/date_time_picker.js
+++ frontline_selenium/filling_scripts/date_time_picker.js
@@ -1,31 +1,22 @@
 $(".form input[data-role='datePicker']").each(function () {
     var datepicker = $(this).data("kendoDatePicker");
     
-    // Generate realistic date: current_year-5 to current_year+1
     var currentYear = new Date().getFullYear();
-    var startYear = currentYear - 5;
-    var endYear = currentYear + 1;
     
     // Create date range
-    var startDate = new Date(startYear, 0, 1); // January 1st of start year
-    var endDate = new Date(endYear, 11, 31);   // December 31st of end year
+    var startDate = new Date(currentYear - 2, 0, 1);
+    var endDate = new Date(currentYear + 1, 11, 31);
     
     // Generate random date in range
-    var timeBetween = endDate.getTime() - startDate.getTime();
-    var randomTime = Math.random() * timeBetween;
-    var randomDate = new Date(startDate.getTime() + randomTime);
+    var randomDate = new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
     
     // Check if the generated date respects datepicker's min/max constraints
     var minDate = datepicker.options.min ? new Date(datepicker.options.min) : null;
     var maxDate = datepicker.options.max ? new Date(datepicker.options.max) : null;
     
     // Adjust if outside constraints
-    if (minDate && randomDate < minDate) {
-        randomDate = minDate;
-    }
-    if (maxDate && randomDate > maxDate) {
-        randomDate = maxDate;
-    }
+    if (minDate && randomDate < minDate) randomDate = minDate;
+    if (maxDate && randomDate > maxDate) randomDate = maxDate;
     
     datepicker.value(randomDate);
     datepicker.trigger('change');
